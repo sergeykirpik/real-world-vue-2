1b75bff366ee28d4cec5e6c259de17571340b089
Sergey Kirpik <sergeykirpik@gmail.com>
2021-05-10 16:44:20 +0300

    Without Vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Globally register all `_base`-prefixed components
+import './components/_globals'
+
 Vue.config.productionTip = false
 
 new Vue({
